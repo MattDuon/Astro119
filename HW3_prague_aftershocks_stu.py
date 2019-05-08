c54d3a6744d9d4493f1e00c209e6ab2a48fea210
MattDuon <49295976+MattDuon@users.noreply.github.com>
2019-05-07 22:32:59 -0700

    Add files via upload

--- HW3_prague_aftershocks_stu.py
+++ HW3_prague_aftershocks_stu.py
@@ -10,18 +10,22 @@ Note to students: If you use the template go through the code
                     line-by-line and comment-out the potion of
                     the code you are not working on
 
+@author: maduong
 """
-import os
+
+#import os
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy.stats
+#import scipy.stats
 #------------my modules-----------------------
 import seis_utils as seis_utils
 import opt_utils as opt_utils
 #--------------------------0---------------------------------------------
 #                     params, dirs, files
 #------------------------------------------------------------------------
+#data_dir = ???
 file_in  = 'prague_aftershock_clean.txt'
+plotFile  = 'Aftershock_plot.png' #file to save plot
 
 dPar  =  { 'k'    : 5, # TODO: change this parameter to see how fit and R2 will changes
            'MSmag': 5.7, #for a specific MS event having the event ID would be better, but MAG works here
@@ -30,7 +34,7 @@ dPar  =  { 'k'    : 5, # TODO: change this parameter to see how fit and R2 will 
            #       the data can be described by a powerlaw
            #  time range for PL fit
            'tmin'  : 1, 'tmax' : 100,
-           'testPlot' : True, # set to False after initial debugging
+           'testPlot' : False, # set to False after initial debugging
            }
 
 #--------------------------1---------------------------------------------
@@ -44,26 +48,24 @@ print( 'total no. of eqs.', mData[0].shape[0])
 #                  initial processing steps
 #------------------------------------------------------------------------
 #TODO: compute max. distance from mainshock for aftershock selection, this is the eq. at the top of page 3
-dPar['rmax'] = ???
+dPar['rmax'] = 10**(.25*dPar['MSmag']-.22)
 #A# select events within certain radius from MS
 MS_ID = np.arange( mData[0].shape[0])[mData[9] == dPar['MSmag']][0]
 #TODO:  compute the distance from mainshock to each event using: seis_utils.haversine (or a basemap equal-distance projection)
-aR    = seis_utils.haversine(-97.1850, 35.6350, mData[7], mData[6])
+aR    = seis_utils.haversine(mData[7][MS_ID], mData[6][MS_ID], mData[7], mData[6])
 #TODO: select events within rmax, i.e. dPar['rmax']
-selR  = ???
+selR  = dPar['rmax'] >= aR
 print( 'maximum radius', dPar['rmax'], 'N  events within r_max: ', selR.sum(), 'N events outside of Rmax', (1-selR).sum())
 mData = mData.T[selR].T
 
-
 #B# update mainshock ID for smaller data-set
-MS_ID = np.arange( mData[0].shape[0])[mData[9] == dPar['MSmag']]
-
+MS_ID = np.arange( mData[0].shape[0])[mData[9] == dPar['MSmag']][0]
 #C# TODO: create time vector in units of days
-at_days = mData[0]*365.25 + ??? + mData[2]  + mData[3]/24 + ??? + ???
+at_days = mData[0]*365.25 + mData[1]*(365.25/12) + mData[2]  + mData[3]/24 + mData[4]/(60*24) + mData[5]/(60*24*60)
 
 
 #D# TODO: temporal decay rates, dN/dt see eq. page 3 of the assignment
-at_bin, aN_bin = ???
+at_bin, aN_bin = seis_utils.eqRate(at_days, dPar['k'])
 
 if dPar['testPlot'] == True:
     # it is a good idea to create a test plot here for debugging purposes
@@ -84,15 +86,17 @@ if dPar['testPlot'] == True:
 #                    power-law fitting
 #------------------------------------------------------------------------
 # TODO: subtract time of mainshock (at_days[MS_ID]) from aftershock times (at_days[MS_ID+1??]- replace ??) - new vector has only AS with time relative to MS in days
-at_AS = ???
+at_AS = at_days[MS_ID+1::] - at_days[MS_ID]
+
 # TODO: compute AS rates
-at_bin_AS, aN_bin_AS   = ???
+at_bin_AS, aN_bin_AS   = seis_utils.eqRate(at_AS, dPar['k'])
 # select events within tmin and tmax for which the data can be described by a powerlaw
 sel_t = np.logical_and( at_bin_AS >= dPar['tmin'], at_bin_AS <= dPar['tmax'])
 # TODO: fot the powerlaw to the log-transformed data between tmin and tmax
-dPL = opt_utils.lin_LS( ???, ???)
+dPL = opt_utils.lin_LS( np.log(at_bin_AS.T[sel_t].T) , np.log(aN_bin_AS.T[sel_t].T))
 p_omori = dPL['b']
 print 'Omori p-value: ', p_omori
+print 'R2 value: ', dPL['R2'] 
 #TODO: use dN/dt = alpha*t**(-p) to compute the modeled rates
 aOmori_rate = 10**( np.log10( at_bin_AS)*p_omori + dPL['a'])
 
@@ -107,16 +111,16 @@ ax = plt.subplot()
 #         at_bin_AS - binned time vector for rate computation, this vector is smaller than at_AS
 #         at_days   - origin times of all events in days (we won't be using this one anymore)
 # :TODO  plot observed aftershock decay rate
-ax.loglog( ????, ???, 'ko', mfc = 'none', mew = 1.5, label = 'aftershocks, $N_{tot}$=%i'%( at_AS.shape[0]))
+ax.loglog( at_bin_AS, aN_bin_AS , 'ko', mfc = 'none', mew = 1.5, label = 'aftershocks, $N_{tot}$=%i'%( at_bin_AS.shape[0]))
 # :TODO plot modeled rates based on Omori law
-????
+ax.loglog( at_bin_AS, aOmori_rate , 'bo', mfc = 'none', mew = 1.5, label = 'aftershocks, $N_{tot}$=%i'%( at_bin_AS.shape[0]))
 
 ax.legend( loc = 'upper right')
 # TODO: add the correct units for the time vector
-ax.set_xlabel( 'Time [???]')
-ax.set_ylabel( 'events/???')
+ax.set_xlabel( 'Time [days]')
+ax.set_ylabel( 'events/day')
 # TODO: save the file make sure to include labels and the legend including the inverted p-value
-plt.savefig( ????)
+plt.savefig(plotFile, dpi = 150)
 plt.show()
 
 
